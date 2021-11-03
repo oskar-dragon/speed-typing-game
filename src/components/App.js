47641cabad0d157a796c3bee1c5f0db5dd2d4d96
Oskar Dragon <oskartdragon@gmail.com>
2021-11-03 22:42:01 +0000

    Added basic functionality, improved button styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+export default function App() {
+  const [words, setWords] = useState("");
+  const [time, setTime] = useState(15);
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
-  render() {
-    return (
-      <div className="container">
-        <h1 className="game__title">Speed Typing App</h1>
-        <textarea className="game__text" />
-        <h4 className="game__timer">Time remaining:</h4>
-        <button className="game__btn">Start</button>
-        <h2 className="game__word-count">Word Count:</h2>
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (time > 0 && isGameOn) {
+      setTimeout(() => {
+        setTime(prevTime => prevTime - 1);
+      }, 1000);
+    }
+
+    if (time === 0) {
+      setIsGameOn(false);
+    }
+
+    if (time === 0 && !isGameOn) {
+      setWordCount(calculateWordCount(words));
+    }
+  }, [time, isGameOn]);
+
+  const handleChange = e => {
+    setWords(e.target.value);
+  };
+
+  const handleClick = () => {
+    setIsGameOn(prevState => !prevState);
+  };
+
+  const calculateWordCount = text => {
+    const regex = /[^\w]/g;
+    const stringArr = text.trim().split(regex);
+    return stringArr.filter(word => word !== "").length;
+  };
+
+  return (
+    <div className="container">
+      <h1 className="game__title">Speed Typing App</h1>
+      <textarea onChange={handleChange} value={words} className="game__text" />
+      <h4 className="game__timer">Time remaining: {time}</h4>
+      <button onClick={handleClick} className="game__btn">
+        {isGameOn ? "Stop" : "Start"}
+      </button>
+      <h2 className="game__word-count">Word Count: {wordCount}</h2>
+    </div>
+  );
 }
-
-export default App;
