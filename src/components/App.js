469390222932b0ceae4355bd02dec90a63e386bc
Oskar Dragon <oskartdragon@gmail.com>
2021-11-08 22:38:18 +0000

    Removed business logic and implemented custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,16 @@
 import React, { useEffect, useState, useRef } from "react";
+import useWordGame from "../hooks/useWordGame";
 
 export default function App() {
-  const STARTING_TIME = 5;
-
-  const [words, setWords] = useState("");
-  const [time, setTime] = useState(STARTING_TIME);
-  const [isGameOn, setIsGameOn] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-  const textAreaRef = useRef(null);
-
-  const startGame = () => {
-    setIsGameOn(true);
-    setTime(STARTING_TIME);
-    setWords("");
-    setWordCount(0);
-    textAreaRef.current.disabled = false;
-    textAreaRef.current.focus();
-  };
-
-  const endGame = () => {
-    setWordCount(calculateWordCount(words));
-  };
-
-  const calculateWordCount = text => {
-    const regex = /[^\w]/g;
-    const stringArr = text.trim().split(regex);
-    return stringArr.filter(word => word !== "").length;
-  };
-
-  useEffect(() => {
-    if (time > 0 && isGameOn) {
-      setTimeout(() => {
-        setTime(prevTime => prevTime - 1);
-      }, 1000);
-    }
-
-    if (time === 0) {
-      setIsGameOn(false);
-    }
-
-    if (time === 0 && !isGameOn) {
-      endGame();
-    }
-  }, [time, isGameOn]);
-
-  const handleChange = e => {
-    setWords(e.target.value);
-  };
+  const {
+    textAreaRef,
+    handleChange,
+    isGameOn,
+    words,
+    time,
+    startGame,
+    wordCount,
+  } = useWordGame();
 
   return (
     <div className="container">
